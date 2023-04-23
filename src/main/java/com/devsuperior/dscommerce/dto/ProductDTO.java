26f6c3bee70b18ec45113571929c4c3aabc77dca
Rudersonvf <rudersonvf@gmail.com>
2023-04-23 19:22:05 -0300

    heroku ready

---
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -0,0 +1,69 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.Category;
+import com.devsuperior.dscommerce.entities.Product;
+
+import javax.validation.constraints.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDTO {
+
+    private Long id;
+    @Size(message = "Nome precisa ter de 3 a 80 caracteres", min = 3, max = 80)
+    @NotBlank(message = "Campo requerido")
+    private String name;
+    @Size(message = "Descrição precisa ter no mínimo 10 caracteres", min = 10)
+    @NotBlank(message = "Campo requerido")
+    private String description;
+    @NotNull(message = "Campo requirido")
+    @Positive(message = "Preço precisa ser positivo")
+    private Double price;
+    private String imgUrl;
+
+    @NotEmpty(message = "Deve ter pelo menos uma categoria")
+    private List<CategoryDTO> categories = new ArrayList<>();
+
+    public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+    public ProductDTO(Product entity) {
+        id = entity.getId();
+        name = entity.getName();
+        description = entity.getDescription();
+        price = entity.getPrice();
+        imgUrl = entity.getImgUrl();
+        for (Category cat : entity.getCategories()) {
+            categories.add(new CategoryDTO(cat));
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public List<CategoryDTO> getCategories() {
+        return categories;
+    }
+}
